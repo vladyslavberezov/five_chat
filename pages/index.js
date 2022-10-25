2e92f8158ab3be5755dbaebe5040d2e54d3e94ad
vladyslavberezov <vl.berezow@gmail.com>
2022-10-25 15:18:03 +0300

    fix navigation

--- pages/index.js
+++ pages/index.js
@@ -1,97 +1,259 @@
-import * as React from 'react'
-import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
-import Link from '../src/Link'
-import CssBaseline from '@mui/material/CssBaseline'
-import LogoutIcon from '@mui/icons-material/Logout'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import Divider from '@mui/material/Divider'
-import Avatar from '@mui/material/Avatar'
-import Stack from '@mui/material/Stack'
-import { FormControl, Grid, IconButton } from '@mui/material'
-import TextField from '@mui/material/TextField'
-import Paper from '@mui/material/Paper'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import {ListItemAvatar, Menu, MenuItem, Tooltip} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import {Logout, Settings} from "@mui/icons-material";
 
-export default function Index () {
-  return (
-    <React.Fragment>
-      <CssBaseline/>
-      <Link href="/SignIn" color="secondary">
-        sign in page
-      </Link>
-      <Container maxWidth="lg">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}
+const drawerWidth = 300;
+
+function ResponsiveDrawer(props) {
+  const {window} = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const drawer = (
+    <div>
+      <Toolbar sx={{paddingLeft: '16px'}}
+               disableGutters>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{padding: 0}}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{width: 40, height: 40}}>M</Avatar>
+          </IconButton>
+        </Tooltip>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&:before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{horizontal: 'right', vertical: 'top'}}
+          anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
         >
-          <Paper elevation={5}>
-            <Box>
-              <Stack
-                flexDirection={'row'}
-                justifyContent={'flex-end'}
-                gap={3}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"
-                        sx={{ width: 24, height: 24 }}
-                />
-                <LogoutIcon/>
-              </Stack>
-              <Typography variant="h4" gutterBottom>
-                My chat!
-              </Typography>
-              <Divider/>
-              <Grid container spacing={12} alignItems="center">
-                <Grid id="chat-window" xs={12} item>
-                  <List>
-                    <text>123</text>
-                  </List>
-                  <List id="chat-window-messages">
-                    {/*{listChatMessages}*/}
-                    <ListItem></ListItem>
-                  </List>
-                </Grid>
-                <Grid xs={3} item>
-                
-                </Grid>
-                <Grid xs={9} item>
-                
-                </Grid>
-                <Grid xs={3} item>
-                  <FormControl fullWidth>
-                    <TextField
-                      label="Nickname"
-                      variant="outlined"/>
-                  </FormControl>
-                  <IconButton
-                    aria-label="send"
-                    color="primary">
-                  </IconButton>
-                  
-                </Grid>
-                <Grid xs={8} item>
-                  <FormControl fullWidth>
-                    <TextField
-                      label="Type your message..."
-                      variant="outlined"/>
-                  </FormControl>
-                  <IconButton
-                    aria-label="send"
-                    color="primary">
-                  </IconButton>
-  
-                </Grid>
-                
-                
-              </Grid>
-            </Box>
-          </Paper>
-        </Box>
-      </Container>
-    </React.Fragment>
-  )
+          <MenuItem>
+            <Avatar/> Profile
+          </MenuItem>
+          <MenuItem>
+            <ListItemIcon>
+              <Settings fontSize="small"/>
+            </ListItemIcon>
+            Settings
+          </MenuItem>
+          <MenuItem>
+            <ListItemIcon>
+              <Logout fontSize="small"/>
+            </ListItemIcon>
+            Logout
+          </MenuItem>
+        </Menu>
+      </Toolbar>
+      <Divider/>
+      <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/king-charles.jpeg"/>
+          </ListItemAvatar>
+          <ListItemText
+            primary="Brunch this weekend?"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{display: 'inline'}}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Ali Connors
+                </Typography>
+                {" — I'll be in your neighborhood doing errands this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" component="li"/>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Queen Elizabeth" src="/queen.png"/>
+          </ListItemAvatar>
+          <ListItemText
+            primary="Summer BBQ"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{display: 'inline'}}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  to Scott, Alex, Jennifer
+                </Typography>
+                {" — Wish I could come, but I'm out of town this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" component="li"/>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Cindy Baker" src="/zalyzhnuy.jpeg"/>
+          </ListItemAvatar>
+          <ListItemText
+            primary="Oui Oui"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{display: 'inline'}}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Sandra Adams
+                </Typography>
+                {' — Do you have Paris recommendations? Have you ever…'}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+      </List>
+    </div>
+  );
+  const container = window !== undefined ? () => window().document.body : undefined;
+  return (
+    <Box sx={{display: 'flex'}}>
+      <CssBaseline/>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: {sm: `calc(100% - ${drawerWidth}px)`},
+          ml: {sm: `${drawerWidth}px`},
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{mr: 2, display: {sm: 'none'}}}
+          >
+            <MenuIcon/>
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            My Chat
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: {xs: 'block', sm: 'none'},
+            '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: {xs: 'none', sm: 'block'},
+            '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{flexGrow: 1, p: 3, width: {sm: `calc(100% - ${drawerWidth}px)`}}}
+      >
+        <Toolbar/>
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+          Odio morbi quis commodo odio aenean sed adipiscing.
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
 
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
 
-
-
-
-
+export default ResponsiveDrawer;
